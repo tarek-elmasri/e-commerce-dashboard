b028d9f0507396d957448dcc3b2eb54b111858ad
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2023-07-01 22:07:23 +0300

    [feat] adds products api routes

---
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -0,0 +1,146 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+// TODO: check if authentication is required
+export const GET = async (
+  req: Request,
+  { params }: { params: { productId: string } }
+) => {
+  try {
+    const product = await prismadb.product.findUnique({
+      where: {
+        id: params.productId,
+      },
+      include: {
+        images: true,
+        category: true,
+        size: true,
+        color: true,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    return serverError(error, "ERROR_PRODUCT_GET");
+  }
+};
+
+export const PATCH = async (
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) => {
+  try {
+    const body = await req.json();
+    const {
+      name,
+      price,
+      categoryId,
+      colorId,
+      sizeId,
+      images,
+      isFeatured,
+      isArchived,
+    } = body;
+
+    if (
+      !name ||
+      !price ||
+      !categoryId ||
+      !colorId ||
+      !sizeId ||
+      !images ||
+      !images.length
+    ) {
+      return new NextResponse("INVALID PARAMETERS", { status: 400 });
+    }
+
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    const storeByUser = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUser) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    await prismadb.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        name,
+        price,
+        categoryId,
+        sizeId,
+        colorId,
+        isFeatured,
+        isArchived,
+        images: {
+          deleteMany: {},
+        },
+      },
+    });
+
+    const product = await prismadb.product.update({
+      where: { id: params.productId },
+      data: {
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
+      },
+    });
+    return NextResponse.json(product);
+  } catch (error) {
+    return serverError(error, "ERROR_PRODUCT_PATCH");
+  }
+};
+
+export const DELETE = async (
+  _req: Request,
+  {
+    params: { storeId, productId },
+  }: { params: { storeId: string; productId: string } }
+) => {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    const storeByUser = await prismadb.store.findFirst({
+      where: {
+        id: storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUser) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const product = await prismadb.product.delete({
+      where: {
+        id: productId,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    return serverError(error, "ERROR_PRODUCT_DELETE");
+  }
+};
+
+const serverError = (error: unknown, label: string) => {
+  console.log(error, label);
+  return new NextResponse("Server Error", { status: 500 });
+};
---
+++ app/api/[storeId]/products/route.ts
@@ -0,0 +1,120 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export const GET = async (
+  req: Request,
+  { params }: { params: { storeId: string } }
+) => {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    const { searchParams } = new URL(req.url);
+    const categoryId = searchParams.get("categoryId") || undefined;
+    const colorId = searchParams.get("colorId") || undefined;
+    const sizeId = searchParams.get("sizeId") || undefined;
+    const isFeateared = searchParams.get("isFeateared");
+    const isArchived = searchParams.get("isArchived");
+
+    const products = await prismadb.product.findMany({
+      where: {
+        storeId: params.storeId,
+        categoryId,
+        colorId,
+        sizeId,
+        isFeatured: isFeateared ? true : undefined,
+        isArchived: isArchived ? true : undefined,
+      },
+      include: {
+        images: true,
+        category: true,
+        size: true,
+        color: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json(products);
+  } catch (error) {
+    console.log(error, "ERROR_PRODUCTS_GET");
+    return new NextResponse("Server Error", { status: 500 });
+  }
+};
+
+export const POST = async (
+  req: Request,
+  { params }: { params: { storeId: string } }
+) => {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    const body = await req.json();
+
+    const {
+      name,
+      price,
+      categoryId,
+      colorId,
+      sizeId,
+      images,
+      isFeatured,
+      isArchived,
+    } = body;
+
+    if (
+      !name ||
+      !price ||
+      !categoryId ||
+      !colorId ||
+      !sizeId ||
+      !images ||
+      !images.length
+    ) {
+      return new NextResponse("INVALID PARAMETERS", { status: 400 });
+    }
+
+    const storeByUser = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUser) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const product = await prismadb.product.create({
+      data: {
+        name,
+        price,
+        isFeatured,
+        isArchived,
+        colorId,
+        sizeId,
+        categoryId,
+        storeId: storeByUser.id,
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log(error, "ERROR_PRODUCT_POST");
+    return new NextResponse("Server Error", { status: 500 });
+  }
+};
